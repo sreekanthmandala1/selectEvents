c0d9f051dece82aadd6a8303d20c157a1cd5f942
sreekanth mandala <sreekanthmandalaa@gmail.com>
2023-04-14 13:23:17 +0530

    filtered according to slected event

--- selectEvents/src/app/app.component.ts
+++ selectEvents/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'selectEvents';
+  nameData:any
+  dataEl:any
+  eventIndex:any
+  ageData:any
+  ageEl:any
+  ageNgIf:boolean = false
+  nameNgIf:boolean = false
+  ngOnInit(){
+  }
+  data : any=[
+    {name : 'srekanth' , age : 100},
+    {name : 'abcdefg' , age : 200},
+  ]
+  onDataChange(e:any){
+    debugger
+  this.eventIndex = e.target.value
+  if(this.eventIndex == 'Name'){
+    this.nameNgIf = true
+    this.ageNgIf = false
+  }
+  if(this.eventIndex == 'Age'){
+    this.ageNgIf = true
+    this.nameNgIf = false
+
+  }
+  }
 }
